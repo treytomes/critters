8736584ce86c7b6526bc06620d90a75cca99e69a
Trey Tomes <george.tomes@cityelectricsupply.com>
2025-04-21 16:56:58 -0500

    chore: 1 class / file

--- States/Particles/ParticleSim.cs
+++ States/Particles/ParticleSim.cs
@@ -7,22 +7,6 @@ using System.Diagnostics;
 
 namespace Critters.States.Particles;
 
-/// <summary>
-/// Simulation parameters for the particle system
-/// </summary>
-class ParticleSimParams
-{
-	public Vector2 Gravity { get; set; } = Vector2.Zero;
-	public Vector2 Bounds { get; set; } = new Vector2(800, 600);
-	public Vector2 Attractor { get; set; } = Vector2.Zero;
-	public float AttractorStrength { get; set; } = 0.0f;
-	public float DampingFactor { get; set; } = 0.99f;
-	public float BounceEnergyLoss { get; set; } = 0.8f;
-	public float MaxAcceleration { get; set; } = 500.0f;
-	public float MaxForce { get; set; } = 1000.0f;
-	public float MinAttractorDistance { get; set; } = 10.0f;
-}
-
 class ParticlesSim : IDisposable
 {
 	#region Constants
